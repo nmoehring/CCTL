b6022db6954a5593c5359c8f6fb784996441d93c
Nathaniel Moehring <nathan.moehring@gmail.com>
2025-03-10 16:58:37 -0500

    Adding a wrapper for std::list to overload operator[].

--- CCUtilities.hpp
+++ CCUtilities.hpp
@@ -8,4 +8,29 @@
 namespace CCUtilities
 {
     std::vector<char> readBinaryFile(std::filesystem::path filename);
+
+    template <typename T>
+    class ListWrapper
+    {
+    public:
+        std::list<T> data;
+
+        T &operator[](std::size_t index)
+        {
+            if (deviceIdx >= m_numDevices)
+                throw std::runtime_error("Device index out of range.");
+            std::list<VknDevice>::iterator it = m_devices.begin();
+            std::advance(it, index);
+            return *it;
+        }
+
+        const T &operator[](std::size_t index) const
+        {
+            if (deviceIdx >= m_numDevices)
+                throw std::runtime_error("Device index out of range.");
+            std::list<VknDevice>::iterator it = m_devices.begin();
+            std::advance(it, index);
+            return *it;
+        }
+    };
 }
